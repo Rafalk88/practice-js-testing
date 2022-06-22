871c853416d10c8e82cc995c8983e669661772f5
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-06-22 18:51:21 +0200

    task 3 second test added

--- 03/app.test.js
+++ 03/app.test.js
@@ -1,7 +1,13 @@
 import randomNumber from './app';
 
-test('random number from 1 to 1 returns 1', () => {
+test("random number from 1 to 1 returns 1", () => {
 
     expect(randomNumber(1,1)).toBe(1)
+
+})
+
+test("min & max are typeof Number", () => {
+
+    expect(randomNumber('a', 2)).toStrictEqual(new TypeError('Wpisz liczby!'))
 
 })
