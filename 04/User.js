d1cb25009ac0c4535b54490dd274d7f6f1e2cfc6
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-06-24 17:34:23 +0200

    task 4 test 1 success

--- 04/User.js
+++ 04/User.js
@@ -1,3 +1,32 @@
 export default class User {
 
+    constructor(object) {
+
+        const {email, password} = object
+
+        this.setProp('email', email)
+        this.setProp('password', password)
+
+    }
+
+    setProp(propName, value) {
+
+        this[propName] = value
+
+    }
+
+    getEmail() {
+
+        const {email, password} = this
+        return email
+
+    }
+
+    getPassword() {
+
+        const {email, password} = this
+        return password
+
+    }
+
 }
